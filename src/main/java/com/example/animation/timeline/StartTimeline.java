73de2bfa09e8b6e77df3ccf1485b11db56573f0f
jot84 <jot84@aber.ac.uk>
2024-03-25 16:36:49 +0000

    play button

--- src/main/java/com/example/animation/timeline/StartTimeline.java
+++ src/main/java/com/example/animation/timeline/StartTimeline.java
@@ -5,12 +5,17 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TreeItem;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 public class StartTimeline {
     public HBox startTimeline(){
 
 
         HBox contentBox = new HBox();
+        Line line = new Line(-100.41410827636719, 60.585784912109375, -100.41421508789062, 34.29289245605469);
+        line.setStroke(Color.web("#0060ff")); // Set line color
+        contentBox.getChildren().add(line);
         //contentBox.getChildren().add(treeView); // Add nodes to HBox
         for (int i = 0; i < 30; i ++){
             Button button = new Button();
